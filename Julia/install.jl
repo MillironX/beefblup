22b934dc49869790c280ba25b9907daecdc679d2
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2019-11-08 18:46:11 -0800

    Add excel reader dependency to install script

--- Julia/install.jl
+++ Julia/install.jl
@@ -1,3 +1,12 @@
+# beefblup install
+# Prepares the Julia environment for using beefblup by installing the requisite
+# packages
+# Usage: julia install.jl
+# (C) 2019 Thomas A. Christensen II
+# Licensed under BSD-3-Clause License
 
+# Import the package manager
 using Pkg
-Pkg.add("XLSXReader")
+
+# Install requisite packages
+Pkg.add("XLSX")
