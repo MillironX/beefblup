05ee8018c2b50386971a84c0be13b6c60d365bc6
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-01-04 02:16:40 -0700

    Remove Matlab script files

--- MATLAB/uniquecell.m
+++
@@ -1,9 +0,0 @@
-% uniquenan
-% Serves the same purpose as UNIQUE, but ensures any empty cells are not
-% counted
-function y = uniquecell(x)
-y = unique(x);
-if any(cellfun(@isempty, y))
-    y(cellfun(@isempty, y)) = [];
-end
-end
