141cf56b07744082590285e88b2069fc8b9eddcf
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-06-19 20:11:25 -0500

    Add basic test for correct fixed effect matrix

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using BeefBLUP
+using DataFrames
 using Test
 
 @testset "BeefBLUP.jl" begin
     # Write your tests here.
+    correctX = [1 1 0 0; 1 1 0 1; 1 0 1 0; 1 0 1 1; 1 0 1 0; 1 0 1 1; 1 0 0 0]
+    fixedfx = DataFrame(year = [1990, 1990, 1991, 1991, 1991, 1991, 1992], sex = ["male", "female", "male", "female", "male", "female", "male"])
+    @test BeefBLUP.fixedeffectmatrix(fixedfx)[1] == correctX
 end
