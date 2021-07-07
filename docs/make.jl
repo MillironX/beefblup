b3162345ffe205245d73efc79850b2df4d1e509f
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2021-07-07 09:33:10 -0500

    Make docs point to this repository

--- docs/make.jl
+++ docs/make.jl
@@ -5,12 +5,12 @@ DocMeta.setdocmeta!(BeefBLUP, :DocTestSetup, :(using BeefBLUP); recursive=true)
 
 makedocs(;
     modules=[BeefBLUP],
-    authors="Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com> and contributors",
-    repo="https://github.com/MillironX/BeefBLUP.jl/blob/{commit}{path}#{line}",
-    sitename="BeefBLUP.jl",
+    authors="Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>",
+    repo="https://github.com/MillironX/beefblup/blob/{commit}{path}#{line}",
+    sitename="beefblup",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://MillironX.github.io/BeefBLUP.jl",
+        canonical="https://millironx.com/beefblup",
         assets=String[],
     ),
     pages=[
